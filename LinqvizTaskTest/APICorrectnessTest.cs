95f2da82823081b4a090e8c072a1f7867497d241
Pavel Suprun <pasha_suprun@mail.ru>
2021-04-12 01:44:52 +0500

    Add files via upload

---
+++ LinqvizTaskTest/APICorrectnessTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using jetbrains_test_linqviz;
+
+namespace LinqvizTaskTest
+{
+    /// <summary>
+    /// Summary description for APICorrectnessTest
+    /// </summary>
+    [TestClass]
+    public class APICorrectnessTest
+    {
+        Controller controller;
+        [TestInitialize]
+        public void SetUp()
+        {
+            controller = new Controller(10, 10);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        [DataRow(1 ,11, 2, 2)]
+        [DataRow(1, 1, 22, 2)]
+        [DataRow(1, 1, 2, 22)]
+        [DataRow(1, -1, 2, 2)]
+        [DataRow(1, 1, -1, 2)]
+        [DataRow(1, 1, 2, -1)]
+        [DataRow(11, 1, 2, 3)]
+        [DataRow(0, 1, 2, 3)]
+        public void TestSettingNonConditionalDepartmentWrongArgument(int dep, int i, int j, int k)
+        {
+            controller.SetSimpleDepartment(dep, i, j, k);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        [DataRow(1, 1, 11, 2, 2, 1, 1, 1)]//i,j,k
+        [DataRow(1, 1, 1, 22, 2, 1, 1, 1)]
+        [DataRow(1, 1, 1, 2, 22, 1, 1, 1)]
+        [DataRow(1, 1, -1, 2, 2, 1, 1, 1)]
+        [DataRow(1, 1, 1, -1, 2, 1, 1, 1)]
+        [DataRow(1, 1, 1, 2, -1, 1, 1, 1)]
+        [DataRow(1, 1, 1, 1, 1, 11, 2, 2)]//t,r,p
+        [DataRow(1, 1, 1, 1, 1, 1, 22, 2)]
+        [DataRow(1, 1, 1, 1, 1, 1, 2, 22)]
+        [DataRow(1, 1, 1, 1, 1, -1, 2, 2)]
+        [DataRow(1, 1, 1, 1, 1, 1, -1, 2)]
+        [DataRow(1, 1, 1, 1, 1, 1, 2, -1)]
+        [DataRow(11, 1, 1, 2, 3, 1, 1, 1)]//dep
+        [DataRow(0, 1, 1, 2, 3, 1, 1, 1)]
+        [DataRow(1, 11, 1, 2, 3, 1, 1, 1)]//s
+        [DataRow(1, 0, 1, 2, 3, 1, 1, 1)]
+        public void TestSettingConditionalDepartmentWrongArgument(int dep, int s, int i, int j, int k, int t, int r, int p)
+        {
+            controller.SetConditionalDepartment(dep, s, i, j, k, t, r, p);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        [DataRow(0)]
+        [DataRow(11)]
+        public void TestExecutingRulesWrongArgument(int dep)
+        {
+            controller.GetCheckList(dep);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TestExecutingRulesNotConfigured()
+        {
+            controller.GetCheckList(1);
+        }
+
+
+    }
+}
---
+++ jetbrains_test_linqviz/Controller.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace jetbrains_test_linqviz
+{
+    public class Controller
+    {
+        int n;
+        int m;
+        int i = 0;
+
+        bool[] conditional;
+        int[] conditionS;
+        int[] ruleParameterI;
+        int[] ruleParameterJ;
+        int[] ruleParameterK;
+        int[] ruleParameterT;
+        int[] ruleParameterP;
+        int[] ruleParameterR;
+
+        bool[] configured;
+
+        object __consistencyLock;
+
+        public Controller(int n, int m)
+        {
+            this.n = n;
+            this.m = m;
+
+            configured = new bool[n];
+            conditional = new bool[n];
+            conditionS = new int[n];
+            ruleParameterI = new int[n];
+            ruleParameterJ = new int[n];
+            ruleParameterK = new int[n];
+            ruleParameterT = new int[n];
+            ruleParameterR = new int[n];
+            ruleParameterP = new int[n];
+            __consistencyLock =  new object();
+        }
+        void Execute(int logAfter, HashSet<string> stampsLogSet)
+        {
+            int department = 0;
+            int nextDepartment = 0;
+            bool[] stamps = new bool[m];
+
+            do
+            {
+                department = nextDepartment;
+                if (!configured[department])
+                {
+                    throw new Exception("Character is in undefined department. Configure it.");
+                }
+                bool stampCondition = true;
+                if (conditional[department])
+                {
+                    stampCondition = stamps[conditionS[department]];
+                }
+
+
+                if (stampCondition)
+                {
+                    SetDeleteStamp(stamps, ruleParameterI[department], ruleParameterJ[department]);
+                    nextDepartment = ruleParameterK[department];
+                }
+                else
+                {
+                    SetDeleteStamp(stamps, ruleParameterT[department], ruleParameterR[department]);
+                    nextDepartment = ruleParameterP[department];
+                }
+
+                if (department == logAfter)
+                {
+
+                    string checkList = boolArrayToString(stamps);
+
+                    if (stampsLogSet.Contains(checkList))
+                    {
+                        stampsLogSet.Add("loop");
+                    }
+                    stampsLogSet.Add(checkList);
+                    return;
+
+                }
+            } while (department != n - 1);
+
+            foreach (string str in stampsLogSet)
+            {
+                System.Console.WriteLine(str);
+            }
+        }
+
+        private void SetDeleteStamp(bool[] stamps, int i, int j)
+        {
+            if (i >= 0)
+            {
+                stamps[i] = true;
+            }
+            if (j >= 0)
+            {
+                stamps[j] = false;
+            }
+        }
+
+        public string boolArrayToString(bool[] array)
+        {
+            return array.Aggregate("", (acc, b) => acc + Convert.ToByte(b).ToString());
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dep"></param>
+        /// <param name="i">0 if no stamp</param>
+        /// <param name="j">0 if no stamp</param>
+        /// <param name="k"></param>
+        public void SetSimpleDepartment(int dep, int i, int j, int k)
+        {
+            if (dep > n || dep < 1)
+            {
+                throw new ArgumentException();
+            }
+            if (i > m || j > m || k > n || i < 0 || j < 0 || k < 1)
+            {
+                throw new ArgumentException();
+            }
+            lock (__consistencyLock)
+            {
+                conditional[--dep] = false;
+                ruleParameterI[dep] = --i;
+                ruleParameterJ[dep] = --j;
+                ruleParameterK[dep] = --k;
+
+                configured[dep] = true;
+            }
+        }
+
+        /// <summary>
+        /// that's what I call a convenient API
+        /// </summary>
+        /// <param name="dep"></param>
+        /// <param name="s"></param>
+        /// <param name="i">0 if no stamp</param>
+        /// <param name="j">0 if no stamp</param>
+        /// <param name="k"></param>
+        /// <param name="t">0 if no stamp</param>
+        /// <param name="r">0 if no stamp</param>
+        /// <param name="p"></param>
+        public void SetConditionalDepartment(int dep, int s, int i, int j, int k, int t, int r, int p)
+        {
+            if (dep > n || dep < 1)
+            {
+                throw new ArgumentException();
+            }
+            if (i > m || j > m || k > n || i < 0 || j < 0 || k < 1)
+            {
+                throw new ArgumentException();
+            }
+            if (t > m || r > m || p > n || t < 0 || r < 0 || p < 1)
+            {
+                throw new ArgumentException();
+            }
+            if (s > m || s < 1)
+            {
+                throw new ArgumentException();
+            }
+
+            lock (__consistencyLock)
+            {
+                conditional[--dep] = true;
+                conditionS[dep] = --s;
+                ruleParameterI[dep] = --i;
+                ruleParameterJ[dep] = --j;
+                ruleParameterK[dep] = --k;
+                ruleParameterT[dep] = --t;
+                ruleParameterR[dep] = --r;
+                ruleParameterP[dep] = --p;
+
+                configured[dep] = true;
+            }
+        }
+        public HashSet<string> GetCheckList(int departmentNumber)
+        {
+            if (departmentNumber > n || departmentNumber < 1)
+            {
+                throw new ArgumentException();
+            }
+            departmentNumber--;
+            HashSet<string> stampsLogSet = new HashSet<string>();
+            lock (__consistencyLock)
+            {
+                Execute(departmentNumber, stampsLogSet);
+            }
+            return stampsLogSet;
+        }
+
+    }
+}
